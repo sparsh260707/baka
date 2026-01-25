e5f387188619e79a1d6dbfd250018dedd9b6c6c8
sparsh260707 <125281480+sparsh260707@users.noreply.github.com>
2026-01-25 14:00:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,9 @@
-BOT_TOKEN = "PASTE_YOUR_BOT_TOKEN_HERE"
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+if not BOT_TOKEN:
+    raise Exception("BOT_TOKEN not found in .env file")
