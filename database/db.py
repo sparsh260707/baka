4b86056efaaee98722f280b9e2cbddcd343aa8b8
sparsh260707 <125281480+sparsh260707@users.noreply.github.com>
2026-01-25 17:17:31 +0530

    Update db.py

--- database/db.py
+++ database/db.py
@@ -13,16 +13,24 @@ def save(data):
     with open(DB_FILE, "w") as f:
         json.dump(data, f, indent=2)
 
-def get_user(data, user):
+def get_user(data, user, chat_id=None):
     uid = str(user.id)
+
     if uid not in data:
         data[uid] = {
             "name": user.first_name,
-            "bal": 200,        # default balance 200
+            "bal": 200,
             "dead_until": 0,
             "protect_until": 0,
             "last_salary": 0,
             "kills": 0,
-            "rob": 0
+            "rob": 0,
+            "groups": []
         }
+
+    # Save group info
+    if chat_id is not None:
+        if chat_id not in data[uid]["groups"]:
+            data[uid]["groups"].append(chat_id)
+
     return data[uid]
